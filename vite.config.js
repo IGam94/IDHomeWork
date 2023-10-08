539b03220e94067e82b0c530e9557ac50a6c8588
JS <you@example.com>
2023-10-08 22:54:39 +0900

    feat: fix build option

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/homeWork/',
+  base: '/IDHomeWork/',
   build: {
     outDir: './docs'
   }
